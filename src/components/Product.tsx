71b95303ab0eaa40084f1c2049c19eaa56f50db9
JSA-Code <jsafortech+github@gmail.com>
2024-11-29 14:21:06 -0800

    add badge component used for pricing, ribbon, discount, etc add DiscountBadge component for discounts add getFormattedPrice function and display badges add formatCurrency function to utils

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,9 @@
 import { products } from "@wix/stores";
 import Link from "next/link";
 import WixImage from "./WixImage";
+import Badge from "./ui/badge";
+import { formatCurrency } from "@/lib/utils";
+import DiscountBadge from "./DiscountBadge";
 
 interface ProductProps {
   product: products.Product;
@@ -10,7 +13,7 @@ export default function Product({ product }: ProductProps) {
   const mainImage = product.media?.mainMedia?.image;
 
   return (
-    <Link className="h-full border" href={`/products/${product.slug}`}>
+    <Link className="h-full border bg-card" href={`/products/${product.slug}`}>
       <div className="relative overflow-hidden">
         <WixImage
           className="transition-transform duration-300 hover:scale-105"
@@ -19,8 +22,12 @@ export default function Product({ product }: ProductProps) {
           width={700}
           height={700}
         />
-        <div className="aboslute bottom-3 right-3 flex flex-wrap items-center gap-2">
-          Here
+        <div className="absolute bottom-3 right-3 flex flex-wrap items-center gap-2">
+          {product.ribbon && <Badge>{product.ribbon}</Badge>}
+          {product.discount && <DiscountBadge data={product.discount} />}
+          <Badge className="bg-secondary font-semibold text-secondary-foreground">
+            {getFormattedPrice(product)}
+          </Badge>
         </div>
       </div>
       <div className="space-y-3 p-3">
@@ -32,4 +39,19 @@ export default function Product({ product }: ProductProps) {
       </div>
     </Link>
   );
+}
+
+function getFormattedPrice(product: products.Product) {
+  const minPrice = product.priceRange?.minValue;
+  const maxPrice = product.priceRange?.maxValue;
+
+  if (minPrice && maxPrice && minPrice !== maxPrice) {
+    return `from ${formatCurrency(minPrice, product.priceData?.currency)}`;
+  } else {
+    return (
+      product.priceData?.formatted?.discountedPrice ||
+      product.priceData?.formatted?.price ||
+      "n/a"
+    );
+  }
 }
