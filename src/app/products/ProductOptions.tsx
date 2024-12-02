3845fbf6572441d2f47b1da48eb0f19e34a8ce87
JSA-Code <jsafortech+github@gmail.com>
2024-12-02 15:00:19 -0800

    add findVariant, checkInStock functions to utils.ts add useState hook to track quantity and selectedOptions with initial state of the first option add ProductOptions component which grants the user options to select from and logic to check if in stock

--- src/app/products/ProductOptions.tsx
+++
@@ -1,9 +0,0 @@
-import { products } from "@wix/stores";
-
-interface ProductOptionsProps {
-  product: products.Product;
-}
-
-export default function ProductOptions({ product }: ProductOptionsProps) {
-  return <div>ProductOptions</div>;
-}
