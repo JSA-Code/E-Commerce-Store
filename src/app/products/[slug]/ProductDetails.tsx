c18c6add30d2dc81c7e1c0b1f5fc56b71922e508
JSA-Code <jsafortech+github@gmail.com>
2024-12-03 11:39:17 -0800

    update getProductBySlug to use cache as inside several pages have multiple requests for same call but will not use same cache for other page calls add ProductPrice component to display price, discount, etc update ProductDetails component's import and use ProductPrice component update dynamic [slug] product page to use generateMetadata, delay data, and add loading skeleton for product details add global app loading spinner enable next.config.ts dynamic data cache

--- src/app/products/[slug]/ProductDetails.tsx
+++ src/app/products/[slug]/ProductDetails.tsx
@@ -3,9 +3,10 @@
 import Badge from "@/components/ui/badge";
 import WixImage from "@/components/WixImage";
 import { products } from "@wix/stores";
-import ProductOptions from "../ProductOptions";
+import ProductOptions from "@/app/products/[slug]/ProductOptions";
 import { useState } from "react";
 import { checkInStock, findVariant } from "@/lib/utils";
+import ProductPrice from "./ProductPrice";
 
 interface ProductDetailsProps {
   product: products.Product;
@@ -22,7 +23,6 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
       }))
       ?.reduce((acc, curr) => ({ ...acc, ...curr }), {}) || {},
   );
-
   const selectedVariant = findVariant(product, selectedOptions);
   const inStock = checkInStock(product, selectedOptions);
 
@@ -37,7 +37,7 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
           height={1000}
         />
       </div>
-      <div className="basis-3/5">
+      <div className="basis-3/5 space-y-5">
         <div className="space-y-2.5">
           <h1 className="text-3xl font-bold lg:text-4xl">{product.name}</h1>
           {product.brand && (
@@ -51,6 +51,7 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
             dangerouslySetInnerHTML={{ __html: product.description }}
           />
         )}
+        <ProductPrice product={product} selectedVariant={selectedVariant} />
         <ProductOptions
           product={product}
           selectedOptions={selectedOptions}
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,13 +1,44 @@
 import { getProductBySlug } from "@/wix-api/products";
 import { notFound } from "next/navigation";
 import ProductDetails from "./ProductDetails";
+import { Metadata } from "next";
+import { delay } from "@/lib/utils";
 
 interface PageProps {
   params: { slug: string };
 }
 
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const product = await getProductBySlug(slug);
+
+  if (!product?._id) notFound();
+
+  const mainImage = product.media?.mainMedia?.image;
+
+  return {
+    title: product.name,
+    description: "Get this product on Awesome Sauce Shop",
+    openGraph: {
+      images: mainImage?.url
+        ? [
+            {
+              url: mainImage.url,
+              width: mainImage.width,
+              height: mainImage.height,
+              alt: mainImage.altText || "",
+            },
+          ]
+        : undefined,
+    },
+  };
+}
+
 export default async function Page({ params }: PageProps) {
-  // ? is this correct? docs says so
+  await delay(2000);
+  // ? is this correct to use "await"? docs says so
   const { slug } = await params;
   const product = await getProductBySlug(slug);
 
@@ -16,7 +47,6 @@ export default async function Page({ params }: PageProps) {
   return (
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
       <ProductDetails product={product} />
-      <pre>{JSON.stringify(product, null, 2)}</pre>
     </main>
   );
 }
