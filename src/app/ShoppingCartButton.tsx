532e64d9dfc4abbef760a89eeecc2b8641a1ae5d
JSA-Code <jsafortech+github@gmail.com>
2024-12-14 20:04:12 -0800

    update: add sheet and display some items, NOT DONE

--- src/app/ShoppingCartButton.tsx
+++ src/app/ShoppingCartButton.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import WixImage from "@/components/WixImage";
 import { useCart } from "@/hooks/cart";
 import { currentCart } from "@wix/ecom";
 import { ShoppingCartIcon } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
 
 interface ShoppingCartButtonProps {
@@ -22,14 +30,92 @@ export default function ShoppingCartButton({
     ) || 0;
 
   return (
-    <div className="relative">
-      <Button variant="ghost" size="icon" onClick={() => setSheetOpen(true)}>
-        {/* TODO inc size shopping cart */}
-        <ShoppingCartIcon />
-        <span className="absolute right-0 top-0 flex size-5 items-center justify-center rounded-full bg-primary text-xs text-primary-foreground">
-          {totalQuantity < 10 ? totalQuantity : "9+"}
-        </span>
-      </Button>
-    </div>
+    <>
+      <div className="relative">
+        <Button variant="ghost" size="icon" onClick={() => setSheetOpen(true)}>
+          {/* TODO inc size shopping cart */}
+          <ShoppingCartIcon />
+          <span className="absolute right-0 top-0 flex size-5 items-center justify-center rounded-full bg-primary text-xs text-primary-foreground">
+            {totalQuantity < 10 ? totalQuantity : "9+"}
+          </span>
+        </Button>
+      </div>
+      <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+        <SheetContent className="flex flex-col sm:max-w-lg">
+          <SheetHeader>
+            <SheetTitle>
+              Your cart{" "}
+              <span className="text-base">
+                ({totalQuantity} {totalQuantity === 1 ? "item" : "items"})
+              </span>
+            </SheetTitle>
+          </SheetHeader>
+          <div className="flex grow flex-col space-y-5 overflow-y-auto">
+            <ul className="space-y-5">
+              {cartQuery.data?.lineItems?.map((item) => (
+                <ShoppingCartItem key={item._id} item={item} />
+              ))}
+            </ul>
+            <pre>{JSON.stringify(cartQuery.data, null, 2)}</pre>
+          </div>
+          <div className="flex items-center justify-between gap-5">
+            <div className="space-y-0.5">
+              <p className="text-sm">Subtotal amount</p>
+              <p className="font-bold">
+                {/* @ts-expect-error wix sdk type does not include subtotal for some reason */}
+                {cartQuery.data?.subtotal?.formattedConvertedAmount}
+              </p>
+              <p className="text-xs text-muted-foreground">
+                Shipping fees and taxes are calculated at checkout
+              </p>
+            </div>
+            <div>
+              <Button size="lg">Checkout</Button>
+            </div>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
+
+interface ShoppingCartItemProps {
+  item: currentCart.LineItem;
+}
+
+function ShoppingCartItem({ item }: ShoppingCartItemProps) {
+  const slug = item.url?.split("/").pop();
+  const quantityLimitReached =
+    !!item.quantity &&
+    !!item.availability?.quantityAvailable &&
+    item.quantity >= item.availability.quantityAvailable;
+
+  return (
+    <li className="flex items-center gap-3">
+      <Link href={`/products/${slug}`}>
+        <WixImage
+          className="flex-none bg-secondary"
+          mediaIdentifier={item.image}
+          width={110}
+          height={110}
+          alt={item.productName?.translated || "Product image"}
+        />
+      </Link>
+      <div className="space-y-1.5 text-sm">
+        <Link href={`/products/${slug}`}>
+          <p className="font-bold">{item.productName?.translated || "Item"}</p>
+        </Link>
+        {!!item.descriptionLines?.length && (
+          <p>
+            {item.descriptionLines
+              .map(
+                (line) =>
+                  line.colorInfo?.translated || line.plainText?.translated,
+              )
+              .join(", ")}
+          </p>
+        )}
+      </div>
+    </li>
   );
 }
